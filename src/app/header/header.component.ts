c8db4e61a0d5dd575e5b8f240dcd495537372d15
AmalRaj9061 <amalrajodattu@gmail.com>
2023-06-22 10:40:20 +0530

    admindashboard

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+
+
+
+// user defined event creation
+// @output - to share data from child to parent
+@Output() onMenuBtnClick = new EventEmitter()
+menubtnClicked(){
+// to occur an userdefined event
+this.onMenuBtnClick.emit()
+
+
+  }
+
+}
